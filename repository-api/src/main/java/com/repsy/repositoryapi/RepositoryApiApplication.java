0d6f29b7f84d8d958d8d64fa1f00285446ecafda
emirhanusta <emirhan1usta@gmail.com>
2025-04-23 22:23:36 +0300

    feat: initialize modular storage architecture with file-system and object storage support

---
+++ repository-api/src/main/java/com/repsy/repositoryapi/RepositoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.repsy.repositoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryApiApplication.class, args);
+    }
+
+}
+    
---
+++ repository-api/src/test/java/com/repsy/repositoryapi/RepositoryApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.repsy.repositoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assert true;
+    }
+
+}
