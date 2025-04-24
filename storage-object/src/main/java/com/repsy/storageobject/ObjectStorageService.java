c4b57c6ef7fb6d09129d1868ce600b83cccc7553
emirhanusta <emirhan1usta@gmail.com>
2025-04-24 16:28:10 +0300

    feat: add logs to ObjectStorageService

--- storage-object/src/main/java/com/repsy/storageobject/ObjectStorageService.java
+++ storage-object/src/main/java/com/repsy/storageobject/ObjectStorageService.java
@@ -3,9 +3,12 @@ package com.repsy.storageobject;
 
 import com.repsy.storagecore.StorageService;
 import com.repsy.storagecore.exception.FileStorageException;
+import com.repsy.storagecore.exception.ResourceNotFoundException;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.GetObjectArgs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.InputStream;
 
 public class ObjectStorageService implements StorageService {
+    private final Logger log = LoggerFactory.getLogger(ObjectStorageService.class);
 
     private final MinioClient minioClient;
     private final String bucketName;
@@ -27,30 +31,44 @@ public class ObjectStorageService implements StorageService {
 
     @Override
     public void save(String packageName, String version, String fileName, MultipartFile file) {
-        try (InputStream inputStream = file.getInputStream()) {
-            String objectName = packageName + "/" + version + "/" + fileName;
-            minioClient.putObject(PutObjectArgs.builder()
-                    .bucket(bucketName)
-                    .object(objectName)
-                    .stream(inputStream, file.getSize(), -1)
-                    .contentType(file.getContentType())
-                    .build());
+        String objectName = packageName + "/" + version + "/" + fileName;
+        log.info("Saving object to MinIO: bucket={}, object={}", bucketName, objectName);
+
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+            log.info("Successfully saved object: {}", objectName);
         } catch (Exception e) {
-            throw new FileStorageException("File could not be saved to MinIO: " + fileName);
+            log.error("Error while saving object to MinIO: {}", objectName, e);
+            throw new FileStorageException("Failed to save object: " + objectName);
         }
     }
 
+
     @Override
     public Resource load(String packageName, String version, String fileName) {
+        String objectName = packageName + "/" + version + "/" + fileName;
+        log.info("Loading object from MinIO: bucket={}, object={}", bucketName, objectName);
+
         try {
-            String objectName = packageName + "/" + version + "/" + fileName;
-            InputStream stream = minioClient.getObject(GetObjectArgs.builder()
-                    .bucket(bucketName)
-                    .object(objectName)
-                    .build());
+            InputStream stream = minioClient.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .build()
+            );
+            log.info("Successfully loaded object: {}", objectName);
             return new InputStreamResource(stream);
         } catch (Exception e) {
-            throw new FileStorageException("File could not be loaded from MinIO: " + fileName);
+            log.warn("Object not found in MinIO: {}", objectName);
+            throw new ResourceNotFoundException("Object not found in MinIO: " + objectName);
         }
     }
+
 }
